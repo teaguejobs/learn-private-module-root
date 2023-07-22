85e31e5752b552351340782a48bd37c9682e9237
teaguejobs <112540374+teaguejobs@users.noreply.github.com>
2023-07-22 01:29:30 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,20 +1,11 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0.0"
-    }
-  }
-}
-
 provider "aws" {
   region = var.region
 }
 
 module "s3-webapp" {
   source  = "app.terraform.io/teague-terraform/s3-webapp/aws"
-  name   = var.name
-  region = var.region
-  prefix = var.prefix
+  name    = var.name
+  region  = var.region
+  prefix  = var.prefix
   version = "1.0.0"
 }
