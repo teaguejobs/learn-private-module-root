0440c5b5b7c69c2de2f4c803cc9822a89ec0faf6
teaguejobs <112540374+teaguejobs@users.noreply.github.com>
2023-07-22 08:51:52 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,3 +10,13 @@ module "learn-private-module-aws-s3-webapp" {
   region = var.region
   prefix = var.prefix
 }
+
+module "s3-webapp" {
+  source  = "app.terraform.io/teague-terraform/learn-private-module-aws-s3-webapp/aws"
+  version = "1.0.0"
+  # insert required variables here
+  name   = var.name
+  region = var.region
+  prefix = var.prefix
+}
+
